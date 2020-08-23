3aaa54eb626fde2338da8449a0fca9436410e3d3
uykim3 <uykim3@lotte.net>
2020-08-23 13:15:12 +0900

    first commit

---
+++ JamsilProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JamsilProject
+//
+//  Created by Lotte on 2020/08/22.
+//  Copyright © 2020 jamsil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
