e3baaf1d7ec82a8d2912eb81a4e8e266e0e4f16a
UCHwang <uchwang@lotte.net>
2020-09-03 17:07:49 +0900

    코어ML 로드 및 카메라 세션 개발

--- JamsilProject/ViewController.swift
+++ JamsilProject/ViewController.swift
@@ -6,15 +6,57 @@
 //  Copyright © 2020 jamsil. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var videoPreview: UIView!
+    var videoCapture: VideoCapture!
+    
+    private var poseNetMLModel: MLModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        do {
+            try self.setUpModel()
+        } catch {
+            print(error)
+        }
+       
+        self.setUpCamera()
     }
 
+    func setUpModel() throws {
+        poseNetMLModel = try PoseNetMobileNet075S16FP16(configuration: .init()).model
+    }
+    
+    func setUpCamera() {
+        videoCapture = VideoCapture()
+        videoCapture.delegate = self
+        videoCapture.fps = 30
+        videoCapture.setUp(sessionPreset: .vga640x480) { success in
+            
+            if success {
+                // add preview view on the layer
+                if let previewLayer = self.videoCapture.previewLayer {
+                    DispatchQueue.main.async {
+                        self.videoPreview.layer.addSublayer(previewLayer)
+                        self.videoCapture.previewLayer?.frame = self.videoPreview.bounds
+                    }
+                }
 
+                self.videoCapture.start()
+            }
+        }
+    }
 }
 
+extension ViewController: VideoCaptureDelegate {
+    func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame: CVPixelBuffer, timestamp: CMTime) {
+        
+    }
+}
