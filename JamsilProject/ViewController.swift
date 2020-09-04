4d63b53ad9ad355c59767084c8b984eb83e2c35c
UCHwang <uchwang@lotte.net>
2020-09-04 15:06:22 +0900

    보정 없이 일단 포지션 값 가져 오도록 빌더 개발

--- JamsilProject/ViewController.swift
+++ JamsilProject/ViewController.swift
@@ -13,10 +13,12 @@ import Vision
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var customView: UIView!
     @IBOutlet weak var videoPreview: UIView!
     var videoCapture: VideoCapture!
     
     private var poseNet: PoseNet?
+    private var currentFrame: CGImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,8 @@ class ViewController: UIViewController {
         } catch {
             fatalError("Failed to load model. \(error.localizedDescription)")
         }
+        
+        self.poseNet?.delegate = self
         
         self.setUpCamera()
     }
@@ -55,6 +59,41 @@ extension ViewController: VideoCaptureDelegate {
     func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame: CVPixelBuffer, timestamp: CMTime, image: CGImage?) {
         if let value = image {
             poseNet?.predict(image: value)
+            self.currentFrame = image
         }
     }
+}
+
+extension ViewController: PoseNetDelegate {
+    func poseNet(_ poseNet: PoseNet, didPredict prediction: PoseNetOutPut) {
+        guard let currentFrame = self.currentFrame else {
+            return
+        }
+        let poseBuilderConfiguration = PoseConfiguration()
+        let poseBuilder = PoseBuilder(output: prediction, configuration: poseBuilderConfiguration, inputImage: currentFrame)
+        
+        let poses = [poseBuilder.pose]
+        
+        for (index, pose) in poses.enumerated() where pose.confidence > 0.9 {
+            for joint in pose.joints {
+                
+                //print(joint.value.confidence)
+                //print("\(index) : \(joint.value.name) : \(joint.value.position)")
+                if joint.value.name == .nose {
+                    DispatchQueue.main.async {
+                        print("\(index) : \(joint.value.name) : \(joint.value.position)")
+                        self.customView.frame = CGRect(x: joint.value.position.x, y: joint.value.position.y, width: 10, height: 10)
+                    }
+                }
+
+            }
+        }
+        
+//        
+//        if poses[0].confidence > 0.8 {
+//            print("x : \(poses[0].joints[.leftAnkle]?.position.x)")
+//            print("y : \(poses[0].joints[.leftAnkle]?.position.y)")
+//        }
+    }
+    
 }
